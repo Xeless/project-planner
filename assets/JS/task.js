19281f5aad4a06faf8a967f0e8c5923dbf3fc635
Xeless <tomdelintepro@gmail.com>
2024-05-07 15:14:41 +0200

    add date and sort

--- assets/JS/task.js
+++ assets/JS/task.js
@@ -1,11 +1,18 @@
-const taskForm = document.getElementById("addTaskButton")
+const taskForm = document.getElementById("addTaskButton");
 const taskTitleInput = document.getElementById("taskTitle");
 const taskDescriptionInput = document.getElementById("taskDescription");
 const taskDateInput = document.getElementById("taskDate");
 const todoList = document.getElementById("test");
+let tasks = [];
 
 
-
+function calculeDuree() {
+    const dateActuelle = new Date();
+    const dateChoisie = new Date(taskDateInput.value);
+    const difference = dateChoisie.getTime() - dateActuelle.getTime();
+    const jours = Math.ceil(difference / (1000 * 60 * 60 * 24));
+    return jours;
+}
 
 taskForm.addEventListener("click", function(event) {
     event.preventDefault();
@@ -13,40 +20,79 @@ taskForm.addEventListener("click", function(event) {
     const title = taskTitleInput.value;
     const description = taskDescriptionInput.value;
     const date = taskDateInput.value;
+    const joursRestants = calculeDuree();
 
-    addTaskToTodoList(date, title, description);
+    const newTask = {
+        title: title,
+        description: description,
+        date: new Date(date),
+        joursRestants: joursRestants
+    };
+
+
+    tasks.push(newTask);
+
+    tasks.sort((a, b) => a.date - b.date);
+
+
+    displayTasks();
 });
 
 
+function displayTasks() {
 
+    todoList.innerHTML = "";
 
+    tasks.forEach(task => {
+        const taskElement = document.createElement("div");
+        taskElement.classList.add("bulle-content");
+        taskElement.innerHTML = `
+            <div class="title-container">
+                <h2 class="titre">${task.title}</h2>
+                <i class="fa-solid fa-trash" style="color: #b50808;"></i>
+            </div>
+            <p class="description">${task.description}</p>
+            <div class="date-container">
+                <p class="date"><i class="fa-regular fa-calendar-days"></i> ${task.date.toDateString()}</p>
+                <p class="jours-restants">Jours restants: ${task.joursRestants}</p>
+                <div class="icon-date">
+                    <i class="fa-solid fa-check" style="color: #63E6BE;"></i>
+                    <i class="fa-solid fa-spinner" style="color: #FFD43B;"></i>
+                </div>
+            </div>`;
 
+        const removeIcon = taskElement.querySelector(".fa-trash");
+        removeIcon.addEventListener("click", function() {
+            taskElement.remove();
 
+            tasks = tasks.filter(t => t !== task);
 
+        });
 
-function addTaskToTodoList(date, title, description) {
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("bulle-content");
-    taskElement.innerHTML = `
-        <h2 class="titre">${title}</h2>
-        <p class="description">${description}</p>
-        <div class="date-container">
-        <p class="date">  <i class="fa-regular fa-calendar-days"></i>  ${date}</p>
-        <i class="fa-solid fa-check"></i>
-        </div>
-    `;
+        const doneIcon = taskElement.querySelector(".fa-check")
+        doneIcon.addEventListener("click", function(){
+            const doneChecked = document.getElementById("done-checked")
+            doneChecked.appendChild(taskElement)
 
-    const checkDoneIcon = taskElement.querySelector(".fa-check"); // Sélection de l'icône check-done
-    checkDoneIcon.addEventListener("click", function() {
+            doneIcon.style.display = "none"
+            progressIcon.style.display = "block"
+            tasks = tasks.filter(t => t !== task);
+            displayTasks();
+        })
 
-const doneChecked = document.getElementById("done-checked")
-doneChecked.appendChild(taskElement)
+        const progressIcon = taskElement.querySelector(".fa-spinner")
+        progressIcon.addEventListener("click", function(){
+            const progressElement = document.getElementById("progress")
+            progressElement.appendChild(taskElement)
 
-checkDoneIcon.remove()
+            doneIcon.style.display = "block"
+            progressIcon.style.display = "none"
+            tasks = tasks.filter(t => t !== task);
+            displayTasks();
+
+        })
+
+        // Ajouter l'élément de tâche à la liste
+        todoList.appendChild(taskElement);
     });
-
-
-
-    todoList.appendChild(taskElement);
-
 }
