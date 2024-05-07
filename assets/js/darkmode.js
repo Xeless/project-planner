45fa20138fa55ebd9f6bc472626b2b0c370f0cd8
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-05-07 17:48:37 +0200

    styled modal, stored the filter and the dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -10,8 +10,19 @@ document.body.appendChild(darkModeButton);
 
 const logo = document.querySelector("img");
 
+const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+    if (isDarkModeEnabled) {
+        document.body.classList.add('dark-mode');
+        darkIcon.classList.remove('fa-sun');
+        darkIcon.classList.add('fa-moon');  
+        logo.src = "./assets/images/logo-dark.png";
+    }
+
 darkModeButton.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
+  const isDarkModeEnabled = document.body.classList.contains('dark-mode');
+  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
+
   if (document.body.classList.contains('dark-mode')){
     darkIcon.classList.remove('fa-sun');
     darkIcon.classList.add('fa-moon');  
